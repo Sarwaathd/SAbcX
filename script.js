04b43d8591d46e633503f451b61ad4aeb9e72f20
Sarwaathd <sakibahmmed0028@gmail.com>
2025-12-23 21:52:47 +0600

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,60 @@
-// Select elements
-const welcomeBtn = document.getElementById('welcomeBtn');
-const publishBtn = document.getElementById('publishBtn');
-const backToAboutBtn = document.getElementById('backToAboutBtn');
-const aboutPage = document.getElementById('aboutPage');
-const contentPage = document.getElementById('contentPage');
+let userScore=0;
+let compScore=0;
+const s1=document.querySelector("#s1");
+const s2=document.querySelector("#s2");
+const msg=document.querySelector("#msg");
+const drawGame=()=>{
+    console.log("Game is Draw");
+    msg.innerText="Game is Draw .Plz try again";
+ msg.style.backgroundColor="blue";}
+    let finam=(userWin)=>{
+        if(userWin===true){
+            userScore++
+            s1.innerText=userScore;
+            console.log("you won");
+            msg.innerText="You won";
+            msg.style.backgroundColor="green";
+        }else{
+            compScore++;
+            s2.innerText=compScore;
+            console.log("you lose");
+            msg.innerText="you Lose";
+              msg.style.backgroundColor="red";
+        }
 
-// Navigation logic
-welcomeBtn.addEventListener('click', () => {
-    aboutPage.style.transform = 'translateY(0)';
-});
+    }
 
-publishBtn.addEventListener('click', () => {
-    contentPage.style.transform = 'translateY(0)';
-});
-
-backToAboutBtn.addEventListener('click', () => {
-    contentPage.style.transform = 'translateY(100%)';
-});
+const playGame=(userChoice)=>{
+    console.log("userChoice=",userChoice);
+    const computerChoice=genComputerChoice();
+    console.log("computerChoice=",computerChoice);
+    if(userChoice === computerChoice){
+        drawGame();
+    }else{
+        let userWin=true;
+        if( userChoice==="rock")//computer rock chose korte parbe na baki ache paper ar scissors tahole}
+        {
+           userWin = computerChoice ==="paper"?false:true;
+        }else if( userChoice==="paper")//computer paper chose korte parbe na baki ache rock ar scissors tahole}
+            {
+           userWin = computerChoice==="scissors"?false:true;
+        }else//computer scissors chose korte parbe na baki ache paper ar rock tahole}
+        {
+            userWin =computerChoice==="rock"?false:true;
+        }
+        finam(userWin);
+    }
+    
+}
+const genComputerChoice=()=>{
+    const option=["rock","paper","scissors"];
+    const randmIdc= Math.floor(Math.random()*3);
+    return option[randmIdc];
+} 
+const choice=document.querySelectorAll(".choice");
+choice.forEach((choice)=>{
+    choice.addEventListener("click",() => {
+        let userChoice=choice.getAttribute("id");
+        playGame(userChoice)
+    })
+})
